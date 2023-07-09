a1a12d71d5e422e403674e539ae48eec0aae5b1b
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-07-09 02:32:46 -0300

    Initial Commit

---
+++ teste/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  teste
+//
+//  Created by Boris R on 09/07/23.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
